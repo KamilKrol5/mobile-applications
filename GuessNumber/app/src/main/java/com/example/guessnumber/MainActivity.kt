8f07d0bb617c3cccae8b4b43bd37eb4be99ed01c
Kamil <kamil55525@gmail.com>
2019-03-08 20:31:54 +0100

    Add guess number application

---
+++ GuessNumber/app/src/main/java/com/example/guessnumber/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.guessnumber
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    var numberOfGuesses = 0
+    var gameStarted = false
+    var upperBound = 50
+    var number = Random.nextInt()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun checkClicked(view: View) {
+        if (gameStarted) {
+            if (editText.text.isEmpty()) {
+                infoTextView.text = "Type a number!"
+            } else {
+                numberOfGuesses++
+                editText.text.toString().toInt().let {
+                    when {
+                        it == number -> {
+                            endGame("You have won! Your number is $number \nNumber of guesses: $numberOfGuesses")
+                        }
+                        it < number -> infoTextView.text = "Your number is bigger than $it"
+                        else -> infoTextView.text = "Your number is smaller than $it"
+                    }
+                    editText.setText("")
+                }
+            }
+        } else {
+            startGame()
+        }
+    }
+
+    private fun endGame(message: String) {
+        gameStarted = false
+        editText.visibility = View.INVISIBLE
+        button.text = "START"
+        infoTextView.text = message
+    }
+
+    private fun startGame() {
+        gameStarted = true
+        numberOfGuesses = 0
+        editText.visibility = View.VISIBLE
+        number = (0..upperBound).random()
+        infoTextView.text = "Try to guess your number"
+        button.text = "CHECK"
+    }
+
+    fun optionsClicked(view: View) {
+        if (optionsLayout.visibility == View.VISIBLE) {
+            optionsLayout.visibility = View.INVISIBLE
+            button.visibility = View.VISIBLE
+        } else {
+            optionsLayout.visibility = View.VISIBLE
+            button.visibility = View.INVISIBLE
+        }
+    }
+
+    fun rangeChooseClick(view: View) {
+        when (view) {
+            radioButton025 -> upperBound = 25
+            radioButton050 -> upperBound = 50
+            radioButton0100 -> upperBound =100
+        }
+        endGame("")
+    }
+    fun closeOptions(view: View) {
+        optionsLayout.visibility = View.INVISIBLE
+        button.visibility = View.VISIBLE
+    }
+}
