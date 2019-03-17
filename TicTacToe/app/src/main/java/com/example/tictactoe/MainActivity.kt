a751037537274b7fe2b5a79d97890774b9ce9297
Kamil <kamil55525@gmail.com>
2019-03-18 00:55:33 +0100

    Implement playing, restarting game and winning

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TableRow
@@ -12,23 +13,22 @@ import kotlin.random.Random
 
 const val dimension = 5
 const val buttonMargin = 10
-var buttonSize = 150
+var buttonSize = 250
 
 enum class Player{
     Cross, Circle
 }
 
-data class Tile(var visited : Boolean)
-
 class MainActivity : AppCompatActivity() {
 
+    private var areTilesResponsive = true
     private var player2 = Player.Circle
     private var player1 = Player.Cross
 //    var player2Img = R.drawable.circle
 //    var player1Img = R.drawable.cross
     var pictures = mapOf(
-        player1 to R.drawable.circle,
-        player2 to R.drawable.cross
+        player1 to R.drawable.cross,
+        player2 to R.drawable.circle
     )
     private var currentPlayer = pictures.keys.elementAt((Random.nextInt(Player.values().size)))
     private var tiles = mutableMapOf<Pair<Int,Int>,Player>()
@@ -50,12 +50,18 @@ class MainActivity : AppCompatActivity() {
                     this.layoutParams = layoutParams2
                     setBackgroundResource(R.color.buttonColor)
                     setPadding(0,0,0,0)
-                    //this.setImageResource(R.drawable.circle)
                     this.scaleType = ImageView.ScaleType.FIT_CENTER
                     this.setOnClickListener {
-                        makeMove(this, currentPlayer)
-                        tiles[i to j] = currentPlayer
-                        checkGameState()
+                        if (areTilesResponsive && !tiles.containsKey(i to j)) {
+                            tiles[i to j] = currentPlayer
+                            makeMove(this, currentPlayer)
+                            val winner = checkGameState()
+                            if (winner != null) {
+                                endGame(winner)
+                            } else if (tiles.size == dimension * dimension) {
+                                endGame(null)
+                            }
+                        }
                     }
                 })
             }
@@ -64,8 +70,102 @@ class MainActivity : AppCompatActivity() {
         changeCurrentPlayerImage(pictures.getValue(currentPlayer))
     }
 
-    private fun checkGameState() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun endGame(winner: Player?) {
+        //tableLayout.visibility = View.INVISIBLE
+        turnLabel.visibility = View.INVISIBLE
+        currentPlayerImageView.visibility = View.INVISIBLE
+        winnerTextView.visibility = View.VISIBLE
+        if (winner != null) {
+            winnerTextView.text = "WINNER:"
+            winnerImageView.setImageResource(pictures.getValue(winner))
+            winnerImageView.visibility = View.VISIBLE
+        } else {
+            winnerImageView.visibility = View.INVISIBLE
+            winnerTextView.text = "TIE!"
+        }
+        areTilesResponsive = false
+    }
+
+    private fun checkGameState() : Player? {
+//        var horizontalOwner : Player? = null
+//        var horizontalState = 0
+//        var verticalOwner : Player? = null
+//        var verticalState = 0
+//        var diagonalSEOwner : Player? = null
+//        var diagonalSE = 0
+//        var diagonalSWOwner : Player? = null
+//        var diagonalSW = 0
+        for (i in 1..dimension) {
+            tiles[i to 1]?.also { owner ->
+                if ((2..dimension).all { tiles[i to it] == owner }) {
+                    return owner
+                }
+            }
+
+            tiles[1 to i]?.also { owner ->
+                if ((2..dimension).all { tiles[it to i] == owner }) {
+                    return owner
+                }
+            }
+        }
+        tiles[1 to 1]?.also { owner ->
+            if ((2..dimension).all { tiles[it to it] == owner }) {
+                return owner
+            }
+        }
+        tiles[1 to dimension]?.also { owner ->
+            if ((2..dimension).all { tiles[it to (dimension - it + 1)] == owner }) {
+                return owner
+            }
+        }
+        return null
+//        for (i in 1..dimension) {
+//            verticalOwner = null
+//            horizontalOwner = null
+//            horizontalState = 0
+//            verticalState = 0
+//            for (j in 1..dimension) {
+//                tiles[i to j]?.also {
+//                    if (horizontalOwner == null) {
+//                        horizontalOwner = it
+//                        horizontalState ++
+//                    } else if (it == horizontalOwner) {
+//                        horizontalState ++
+//                    }
+//                }
+//                tiles[j to i]?.also {
+//                    if (verticalOwner == null) {
+//                        verticalOwner = it
+//                        verticalState ++
+//                    } else if (it == verticalOwner) {
+//                        verticalState ++
+//                    }
+//                }
+//            }
+//            tiles[i to i]?.also {
+//                if (diagonalSEOwner == null) {
+//                    diagonalSEOwner = it
+//                    diagonalSE ++
+//                } else if (it == diagonalSEOwner) {
+//                    diagonalSE ++
+//                }
+//            }
+//            tiles[i to (dimension - i + 1)]?.also {
+//                if (diagonalSWOwner == null) {
+//                    diagonalSWOwner = it
+//                    diagonalSW ++
+//                } else if (it == diagonalSWOwner) {
+//                    diagonalSW ++
+//                }
+//            }
+//            when {
+//                verticalState == dimension -> return verticalOwner
+//                horizontalState == dimension -> return horizontalOwner
+//                diagonalSE == dimension -> return diagonalSEOwner
+//                diagonalSW == dimension -> return diagonalSWOwner
+//            }
+//        }
+//        return null
     }
 
     private fun makeMove(button: ImageButton,player: Player) {
@@ -81,11 +181,29 @@ class MainActivity : AppCompatActivity() {
                 player1
             }
         }
-        button.setOnClickListener { }
     }
 
     private fun changeCurrentPlayerImage(image: Int) {
         currentPlayerImageView.setImageResource(image)
+    }
+
+    fun startGame(view: View) {
+        tiles.clear()
+        for (i in 0 until tableLayout.childCount) {
+            val row = tableLayout.getChildAt(i)
+            if (row is TableRow) {
+                for (j in 0 until row.childCount) {
+                    (row.getChildAt(j) as ImageButton).setImageResource(0)
+                }
+            }
+        }
+        turnLabel.visibility = View.VISIBLE
+        currentPlayerImageView.visibility = View.VISIBLE
+        winnerTextView.visibility = View.INVISIBLE
+        winnerImageView.visibility = View.INVISIBLE
+        currentPlayer = if (currentPlayer == player2) player1 else player2
+        changeCurrentPlayerImage(pictures.getValue(currentPlayer))
+        areTilesResponsive = true
     }
 
 }
