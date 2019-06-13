621a2deb6c7ee6dfde5fe18d283f993db9a1adb0
Kamil <kamil55525@gmail.com>
2019-06-14 00:34:26 +0200

    Add fragment with map

---
+++ Cities/app/src/main/java/pl/kamilkrol/cities/MapFragment.kt
@@ -0,0 +1,85 @@
+package pl.kamilkrol.cities
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_map.*
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private var listener: OnFragmentInteractionListener? = null
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        childFragmentManager.findFragmentByTag("map")?.let { mapFragment ->
+            (mapFragment as SupportMapFragment).getMapAsync(this)
+        } ?: run {
+            val mapFragment = SupportMapFragment.newInstance(
+                GoogleMapOptions()
+                    .camera(CameraPosition.fromLatLngZoom(LatLng(51.109286, 17.032307), 5.0f))
+            )
+            childFragmentManager.beginTransaction().replace(R.id.map, mapFragment, "map").commit()
+            mapFragment.getMapAsync(this)
+        }
+        fab.setOnClickListener { addCityClicked() }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+
+    fun addCityClicked() {
+        Toast.makeText(context, "XD", Toast.LENGTH_SHORT).show()
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction()
+    }
+
+}
